7029111293a9272c550a3ef2220c32d7e918674d
Cao Việt Dũng <dung.cao@lifesup.local>
2023-11-08 17:48:22 +0700

    vvV

--- src/app/components/client/sidebar-catgory/sidebar-catgory.component.ts
+++ src/app/components/client/sidebar-catgory/sidebar-catgory.component.ts
@@ -48,9 +48,50 @@ export class SidebarCatgoryComponent implements OnInit {
     },
   ]
 
+  // objectFilter
+
+  objectFilter = {
+    category: '',
+    price: {
+      min: 0,
+      max: 100
+    }
+  }
+
+  rangeValues: number[] = [0, 100];
+
+  maxPrice = 5000000
+
   constructor() {}
 
 
   ngOnInit() {
+    this.objectFilter.price.max = this.maxPrice
+    this.listCategory.map((x:any,index:any)=>{
+      x.checked = false
+      x.id += index
+      return x
+    })
   }
+
+  onChangeFilter(type:any){
+    switch (type) {
+      case 'category':
+        this.objectFilter.category = this.listCategory.filter((x:any)=>{
+          return x.checked === true
+        })
+        console.log(this.objectFilter.category)
+        break;
+      
+      case 'price':
+        this.objectFilter.price.min = Math.floor((this.rangeValues[0] / 100)  * this.maxPrice)
+        this.objectFilter.price.max = Math.floor((this.rangeValues[1] / 100)  * this.maxPrice)
+console.log(this.objectFilter.price)
+        break;
+        
+      default:
+        break;
+    }
+  }
+
 }
--- src/app/components/client/sidebar-catgory/sidebar-catgory.module.ts
+++ src/app/components/client/sidebar-catgory/sidebar-catgory.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarCatgoryComponent } from './sidebar-catgory.component';
+import { CheckboxModule, SliderModule } from 'primeng';
+import { FormsModule } from '@angular/forms';
+import { SharedPipeModule } from 'src/app/shared/sharedPipe.module';
 
 
 
 @NgModule({
   declarations: [SidebarCatgoryComponent],
   imports: [
-    CommonModule
+    CommonModule,CheckboxModule,FormsModule,SliderModule,SharedPipeModule
   ],exports: [SidebarCatgoryComponent]
 })
 export class SidebarCatgoryModule { }
