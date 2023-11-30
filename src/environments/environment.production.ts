f8ac1206dfc9003005725648159f87cdac1b8e7d
Cao Việt Dũng <dung.cao@lifesup.local>
2023-11-30 17:12:51 +0700

    123

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    BASE_URL_BE: 'http://apistore.peooshop.top/api/v2'
+    BASE_URL_BE: 'http://apistore.peooshop.top/api/v2',
+    BASE_URL_BE_Client: 'http://localhost:3000/api/v3'
   };
   
